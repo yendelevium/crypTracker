89cef62569f245b682af861cc800a2893c5ae411
yendelevium <yashbardia27@gmail.com>
2025-02-10 07:15:25 +0000

    Constant DB Updation
    - Persists the scraped data to the DB every 15 seconds
    - Modify gitignore

--- internal/database/db.go
+++ internal/database/db.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/yendelevium/crypTracker/internal/api"
+	"github.com/yendelevium/crypTracker/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -40,31 +41,48 @@ func (dbClient *DBClient) Seed() error {
 	return nil
 }
 
-func (dbClient *DBClient) ScrapeData(wg *sync.WaitGroup) error {
-	defer wg.Done()
+func (dbClient *DBClient) ScrapeData() {
 	data, err := api.FetchCoinData()
 	if err != nil {
 		log.Println(err)
-		return err
-	}
-	// Inserting each element
-	for _, ele := range data {
-		result := dbClient.Client.Model(&ele).Updates(ele)
-		if result.Error != nil {
-			log.Println(result)
-		}
 	}
 
-	return nil
+	// If I run EVERY update concurrently, due to write-write conflict, the DB kindof implements a lock
+	// This makes it counter productive as this makes it take MORE TIME PER QUERY
+	// But, we can limit the concurrent routines to 5 using a buffered channel
+
+	// If multiple goroutines are updating the same table concurrently, lock contention could be slowing things down.
+	// PostgreSQL enforces row-level locks for updates, and if many updates are happening at once,
+	// transactions might queue up, increasing execution time.
+
+	// This works
+	var wg sync.WaitGroup
+	slots := make(chan struct{}, 5)
+
+	for _, ele := range data {
+		wg.Add(1)
+		go func(e models.Coin) {
+			defer wg.Done()
+			slots <- struct{}{} // Acquire a concurrency slot. IF it's full, the channel blocks
+
+			result := dbClient.Client.Model(&e).Updates(e)
+			if result.Error != nil {
+				log.Println(result)
+			}
+
+			<-slots // Release the slot so the next routine can run
+		}(ele)
+	}
+
+	wg.Wait()
 }
 
-func (dbCLient *DBClient) StartScraping() {
+func (dbClient *DBClient) StartScraping() {
 	ticker := time.NewTicker(15 * time.Second)
+	// To immediately start scraping when the function is called, we use <-ticker.C
+	// If we use for range ticker.C, it will first wait for 15s, and THEN start the first scrapw
 	for ; ; <-ticker.C {
-		wg := &sync.WaitGroup{}
 		log.Println("Scraping Data")
-		wg.Add(1)
-		go dbCLient.ScrapeData(wg)
-		wg.Wait()
+		go dbClient.ScrapeData() // No blocking, allows overlap if needed
 	}
 }
